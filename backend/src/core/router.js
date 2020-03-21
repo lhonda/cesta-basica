c4c34c0b81b138610d306a789fefe947c82d0472
Marcus Ortense <marcus.ortense@outlook.com>
2020-03-21 17:59:53 -0300

    fix typo

--- backend/src/core/router.js
+++ backend/src/core/router.js
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import healthCheck from '../rules/health-heck'
+import healthCheck from '../rules/health-check'
 
 export const router = Router()
 router.get('/health-check', (req, res) => res.status(200).json(healthCheck()))

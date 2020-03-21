7f52811b9ca310c81ea0be961862526d0bb6cae5
Marcus Ortense <marcus.ortense@outlook.com>
2020-03-21 19:13:34 -0300

    lint and code style

--- backend/src/core/router.js
+++ backend/src/core/router.js
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import healthCheck from '../rules/health-check'
+import { healthCheck } from '../rules'
 
 export const router = Router()
 router.get('/health-check', (req, res) => res.status(200).json(healthCheck()))
--- backend/src/rules/health-check.js
+++ backend/src/rules/health-check.js
@@ -1,17 +1,21 @@
 import { getConnectionState } from '../core/database'
 
+const SECONDS_PER_HOUR = 3600
+const MINUTES_PER_HOUR = 60
+const SECONDS_PER_MINUTE = 60
+
 export const numberToTimeString = (n) => n.toString().padStart(2, '0')
 
-export function toHHMMSS(uptime) {
-    const uptimeSeconds = Math.round(uptime)
-    const hours = Math.floor(uptimeSeconds / 3600)
-    const minutes = Math.floor((uptimeSeconds - (hours * 3600)) / 60)
-    const seconds = uptimeSeconds - (hours * 3600) - (minutes * 60)
+export function toHHMMSS (uptime) {
+  const uptimeSeconds = Math.round(uptime)
+  const hours = Math.floor(uptimeSeconds / SECONDS_PER_HOUR)
+  const minutes = Math.floor((uptimeSeconds - (hours * SECONDS_PER_HOUR)) / MINUTES_PER_HOUR)
+  const seconds = uptimeSeconds - (hours * SECONDS_PER_HOUR) - (minutes * SECONDS_PER_MINUTE)
 
   return `${numberToTimeString(hours)}:${numberToTimeString(minutes)}:${numberToTimeString(seconds)}`
 }
 
-export default function healthCheck() {
+export function healthCheck () {
   return {
     uptime: toHHMMSS(process.uptime()),
     database: {

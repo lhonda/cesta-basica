b3b3831fd80154005edd87beb036db91480ce734
“Iran <iranjunior94@gmail.com>
2020-03-22 13:54:25 -0300

    feature(initial components): inicializacao dos components que estaram na tela inicial

---
+++ frontend/src/components/Title/Title.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import './Title.scss';
+
+export default function Title( {message} ) {
+  return (<h2 className="title" >{message}</h2>)
+}
---
+++ frontend/src/pages/Terms/Terms.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Title } from '../../components/Title';
+import './Term.scss';
+
+
+export default function Terms() {
+  return (
+    <div className="container">
+        <div className="header">
+            <Title message="Termos de compromisso"/>
+        </div>
+    </div>
+  )
+}

c15ee96ccf5b878f4accd352a4baf8eff5349fa9
Stefani C. Silva <stefani.c.silva@accenture.com>
2020-03-23 17:32:33 -0300

    Added Home component

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -3,6 +3,7 @@ import { Switch, BrowserRouter } from 'react-router-dom'
 
 import Route from './routeWrapper'
 
+import { Home } from '../pages/Home'
 import { Login } from '../pages/Login'
 import { Register } from '../pages/Register'
 import { Terms } from '../pages/Terms'
@@ -11,7 +12,7 @@ import { Checklist } from '../pages/Checklist'
 export const Routes = () => (
   <BrowserRouter>
     <Switch>
-      <Route path="/" exact component={() => <div>root</div>} />
+      <Route path="/" exact component={Home} />
       <Route path="/login" exact component={Login} />
       <Route path="/register" exact component={Register} />
       <Route path="/terms" exact component={Terms} />

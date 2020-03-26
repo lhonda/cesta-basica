9c2fffad7d56d07dc790b04065956517bb5e3df1
Marcus Ortense <marcus.ortense@outlook.com>
2020-03-26 09:07:50 -0300

    remove unnecessary package

--- backend/src/core/server.js
+++ backend/src/core/server.js
@@ -1,22 +1,16 @@
 import express from 'express'
 import fileUpload from 'express-fileupload'
-import serverless from 'serverless-http'
 import helmet from 'helmet'
 import cors from 'cors'
-import bodyParser from 'body-parser'
 import morgan from 'morgan'
 import { databaseConnector } from '../middlewares'
 import { router } from './router'
 
 export const app = express()
-export const handler = serverless(app)
 
 app.use(express.json())
-app.use(fileUpload({
-  createParentPath: true
-}))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }))
+app.use(fileUpload({ createParentPath: true }))
 app.use(morgan('dev'))
 app.use(helmet())
 app.use(cors())

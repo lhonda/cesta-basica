5ec1b61d3e7e8d5a64f9b38d58cd61c51e851f67
Fernando Reis Guimaraes <fernando.g.reis@accenture.com>
2020-03-26 17:13:34 -0300

    data load users  new fields

--- backend/src/load-data/load-types/user.js
+++ backend/src/load-data/load-types/user.js
@@ -1,17 +1,14 @@
 import { connect, disconnect } from '../../core/database'
-import { random } from '../../services'
 import { User } from '../../repositories'
 import { config } from 'dotenv'
 import { genericProcess } from '../genericProcess'
 
 export async function process () {
   return genericProcess(User, 'user-data.csv', row => {
-    const password = random.password()
-
     return {
       login: row.cpf,
-      password: password,
-      initialPassword: password,
+      password: row.password,
+      initialPassword: row.password,
       name: row.name,
       role: 'leader',
       email: row.email,
@@ -21,7 +18,10 @@ export async function process () {
       birthdate: row.birthdate,
       site: row.site,
       city: row.city,
-      state: row.state
+      state: row.state,
+      deliveryLocation: row.deliveryLocation,
+      deliveryCep: row.deliveryCep,
+      slums: row.slums
     }
   })
 }

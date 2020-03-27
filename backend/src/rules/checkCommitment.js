07e9e1af2918914ff015825ad9fb593274b43f53
Fernando Reis Guimaraes <fernando.g.reis@accenture.com>
2020-03-27 16:37:59 -0300

    fix checkcommitment

--- backend/src/rules/checkCommitment.js
+++ backend/src/rules/checkCommitment.js
@@ -3,7 +3,10 @@ import { Events } from '../repositories'
 export async function checkCommitment ({ login }) {
   const commitment = await Events.findOne({ login })
   if (commitment) {
-    return { commitment: true }
+    return {
+      commitment: true,
+      timestamp: commitment.timestamp
+    }
   }
   return { commitment: false }
 }

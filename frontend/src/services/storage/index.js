dbbd27110e2f1219a1b30f02a57ec033b962ccc0
Glaucio Ricardo <glaucio.ricardo.jun@accenture.com>
2020-03-27 16:18:30 -0300

    adjust in validation for view checklist

--- frontend/src/services/storage/index.js
+++ frontend/src/services/storage/index.js
@@ -1,7 +1,7 @@
 import { keys } from './storageKeys'
 
 export const set = (value) => localStorage.setItem(keys.GLOBAL_STORE, JSON.stringify(value))
-export const clear = () => localStorage.clear()
+export const clear = () => localStorage.setItem(keys.GLOBAL_STORE, null)
 export function get() {
   try {
     return JSON.parse(localStorage.getItem(keys.GLOBAL_STORE))

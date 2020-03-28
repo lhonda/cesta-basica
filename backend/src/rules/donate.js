9c5eb08166ac0fa14ee12f1a7c1dee35b19affcb
Fernando Reis Guimaraes <fernando.g.reis@accenture.com>
2020-03-28 14:39:20 -0300

    fix donate

--- backend/src/rules/donate.js
+++ backend/src/rules/donate.js
@@ -14,6 +14,11 @@ export async function donate ({
 }) {
   console.log(arguments)
 
+  const timestamp = new Date()
+
+  /**
+   * Validations
+   */
   if (!donationId) {
     throw new Error('donationId is required')
   }
@@ -34,90 +39,97 @@ export async function donate ({
     throw new Error('delivered is required')
   }
 
-  // convertendo 'true' ou 'false' ou true ou false pra boolean
-  delivered = JSON.parse(delivered)
-
-  if (delivered === false) {
-    const voucher = await Voucher.findOne({ voucherId })
-
-    voucher.status = 3
-    voucher.delivered = new Date()
-    voucher.point = {
-      type: 'Point',
-      coordinates: [lon, lat]
-    }
-    voucher.save()
-    return
-  } else if (delivered !== true) {
-    // todo: implement joi validation
-    throw new Error('delivered must be a boolean value')
-  }
-
-  if (!receivedCpf) {
-    throw new Error('receivedCpf is required')
-  }
-
-  if (!receivedName) {
-    throw new Error('receivedName is required')
-  }
-
-  if (!donateDonationFile) {
-    throw new Error('donateDonationFile is required')
-  }
-
   const donation = await Donation.findOne({ donationId: donationId })
 
   if (!donation) {
     throw new Error(`Could not find the Donation with id: ${donationId}`)
   }
 
-  if (donation) {
-    const voucher = await Voucher.findOne({ voucherId })
+  const voucher = await Voucher.findOne({ voucherId })
 
-    if (!voucher) {
-      throw new Error(`Could not find the voucherId provided: ${voucherId}`)
-    }
+  if (!voucher) {
+    throw new Error(`Could not find the Voucher with id: ${donationId}`)
+  }
 
-    const timestamp = new Date()
-    const [, ext] = donateDonationFile.mimetype.split('/')
-    const key = `provas/entregas/entrega-doacao-${login}-${donationId}-${timestamp.toISOString()}.${ext}`
+  if (donation.leaderLogin !== login) {
+    throw new Error('The owner of this donation is not the same of the token')
+  }
 
-    const params = {
-      Bucket: process.env.BUCKET_NAME,
-      Key: key,
-      Body: donateDonationFile.data
-    }
+  if (voucher.leaderLogin !== login) {
+    throw new Error('The owner of this voucher is not the same of the token')
+  }
 
-    const s3 = new AWS.S3()
-
-    s3.upload(params, function (err, data) {
-      if (err) {
-        throw err
-      }
-      console.log(`File uploaded successfully.Key:${key}`)
-    })
-
-    donation.status = 3
-    donation.lastDelivery = timestamp
-
-    voucher.receivedCpf = receivedCpf
-    voucher.status = 2
-    voucher.receivedName = receivedName
+  /**
+   * Not delivered
+   */
+  if (delivered === false || delivered === 'false') {
+    voucher.status = 3
     voucher.delivered = timestamp
-    voucher.cardDonatedS3Key = key
     voucher.point = {
       type: 'Point',
       coordinates: [lon, lat]
     }
 
-    console.log(donation)
-    console.log(voucher)
-
-    await donation.save()
     await voucher.save()
-
-    return donation
+  } else if (delivered !== true && delivered !== 'true') {
+    throw new Error('delivered must be a boolean value')
   }
 
-  return Promise.reject(new Error(`Donating donation ${donationId} failed.`))
+  /**
+   * More validations when delivered = true
+   */
+  if (!receivedCpf) {
+    throw new Error('receivedCpf is required when delivered = true')
+  }
+
+  if (!receivedName) {
+    throw new Error('receivedName is required when delivered = true')
+  }
+
+  if (!donateDonationFile) {
+    throw new Error('donateDonationFile is required when delivered = true')
+  }
+
+  /**
+   * S3 Upload
+   */
+  const [, ext] = donateDonationFile.mimetype.split('/')
+  const key = `provas/entregas/entrega-doacao-${login}-${donationId}-${timestamp.toISOString()}.${ext}`
+
+  const params = {
+    Bucket: process.env.BUCKET_NAME,
+    Key: key,
+    Body: donateDonationFile.data
+  }
+
+  const s3 = new AWS.S3()
+
+  s3.upload(params, function (err, data) {
+    if (err) {
+      throw err
+    }
+    console.log(`File uploaded successfully.Key:${key}`)
+  })
+
+  /**
+   * Persisting information do mongodb
+   */
+  donation.status = 3
+  donation.lastDelivery = timestamp
+
+  voucher.receivedCpf = receivedCpf
+  voucher.status = 2
+  voucher.receivedName = receivedName
+  voucher.delivered = timestamp
+  voucher.cardDonatedS3Key = key
+  voucher.point = {
+    type: 'Point',
+    coordinates: [lon, lat]
+  }
+
+  console.log(donation)
+  console.log(voucher)
+
+  await donation.save()
+  await voucher.save()
 }

72cbda8f1688d4d25e727e618244274e6178fa9c
Fernando Reis Guimaraes <fernando.g.reis@accenture.com>
2020-03-28 12:15:40 -0300

    fix check commitment - fix listVouchers

--- backend/src/rules/checkCommitment.js
+++ backend/src/rules/checkCommitment.js
@@ -1,7 +1,7 @@
 import { Events } from '../repositories'
 
 export async function checkCommitment ({ login }) {
-  const commitment = await Events.findOne({ login })
+  const commitment = await Events.findOne({ login, type: 'commitment' })
   if (commitment) {
     return {
       commitment: true,

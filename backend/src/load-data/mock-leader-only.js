673c130b97bf71a8e882d54be760be0c61467385
Fernando Reis Guimaraes <fernando.g.reis@accenture.com>
2020-03-31 18:21:32 -0300

    new mock types

---
+++ backend/src/load-data/mock-leader-only.js
@@ -0,0 +1,28 @@
+import { config } from 'dotenv'
+import { connect, disconnect } from '../core/database'
+import { User } from '../repositories'
+
+if (require.main === module) {
+  (async function () {
+    try {
+      config()
+      await connect()
+
+      const login = process.argv.pop()
+      const password = '12345678'
+      const email = `mock-${login}@teste.com`
+      const role = 'leader'
+
+      await User.create({
+        login,
+        password,
+        email,
+        role
+      })
+
+      await disconnect()
+    } catch (err) {
+      console.error(err)
+    }
+  }())
+}

2bee3824e5ff5c75cb8f496ded8927d771c6393d
Leonel Pelusi <cs-leonel-pelusi@accenture.com>
2020-04-01 16:02:21 -0300

    add filters

--- backend/src/rules/filters.js
+++ backend/src/rules/filters.js
@@ -1,6 +1,9 @@
 import { Donation, User, Site } from '../repositories'
 
 export async function filterDonation (donationId) {
+  if (!donationId) {
+    throw new Error('DonationId is required')
+  }
   const donationTip = (await Donation.find({ donationId: new RegExp(`^${donationId}`) }, { donationId: 1, _id: 0 })
   ).map(donation => donation.donationId)
 
@@ -8,6 +11,9 @@ export async function filterDonation (donationId) {
 }
 
 export async function filterSite (name) {
+  if (!name) {
+    throw new Error('Name is required')
+  }
   const siteTip = (await Site.find({ name: new RegExp(`^${name}`) }, { name: 1, _id: 0 })
   ).map(site => site.name)
   console.log(siteTip)
@@ -16,6 +22,9 @@ export async function filterSite (name) {
 }
 
 export async function filterLeader (name) {
+  if (!name) {
+    throw new Error('Name is required')
+  }
   const nameTip = (await User.find({ role: 'leader', name: new RegExp(`^${name}`) }, { name: 1, _id: 0 })
   ).map(leader => leader.name)
 

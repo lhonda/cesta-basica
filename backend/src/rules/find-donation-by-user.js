d144bfaae1b658db421a54ce45498cbace4dbd06
Leonel Pelusi <cs-leonel-pelusi@accenture.com>
2020-04-01 11:00:17 -0300

    upload find-donation-by-user

--- backend/src/rules/find-donation-by-user.js
+++ backend/src/rules/find-donation-by-user.js
@@ -2,8 +2,6 @@ import { findDonation } from './find-donations'
 import { findDonationByLeader } from './find-donation-by-leader'
 
 export async function findDonationByUser (user) {
-  console.log(user)
-  console.log(user.login)
   return user.role === 'admin'
     ? findDonation()
     : findDonationByLeader(user)

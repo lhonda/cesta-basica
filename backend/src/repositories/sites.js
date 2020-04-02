696244ff839dbf99422b2ded80cf5a31df2dfdba
Leonel Pelusi <cs-leonel-pelusi@accenture.com>
2020-04-02 11:52:05 -0300

    add siteId

--- backend/src/repositories/sites.js
+++ backend/src/repositories/sites.js
@@ -1,6 +1,11 @@
 import { model, Schema } from 'mongoose'
 
 const schema = new Schema({
+  siteId: {
+    type: String,
+    unique: true,
+    required: [true, 'siteId is required']
+  },
   name: {
     type: String,
     unique: true,

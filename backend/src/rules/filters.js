08913ffa888a2ef4528cdd81b4c7e6b46a992b3d
Leonel Pelusi <cs-leonel-pelusi@accenture.com>
2020-04-02 11:51:01 -0300

    fix filters

--- backend/src/rules/filters.js
+++ backend/src/rules/filters.js
@@ -4,8 +4,8 @@ export async function filterDonation (donationId) {
   if (!donationId) {
     throw new Error('DonationId is required')
   }
-  const donationTip = (await Donation.find({ donationId: new RegExp(`^${donationId}`) }, { donationId: 1, _id: 0 })
-  ).map(donation => donation.donationId)
+  const donationTip = (await Donation.find({ donationId: new RegExp(`^${donationId}`) }, { donationId: 1 })
+  ).map(({ donationId }) => ({ donationId }))
 
   return donationTip
 }
@@ -14,9 +14,15 @@ export async function filterSite (name) {
   if (!name) {
     throw new Error('Name is required')
   }
-  const siteTip = (await Site.find({ name: new RegExp(`^${name}`) }, { name: 1, _id: 0 })
-  ).map(site => site.name)
-  console.log(siteTip)
+  const siteTip = (await Site.find(
+    {
+      name: new RegExp(`^${name}`)
+    },
+    {
+      siteId: 1,
+      name: 1
+    })
+  ).map(({ name, siteId }) => ({ name, siteId }))
 
   return siteTip
 }
@@ -25,8 +31,16 @@ export async function filterLeader (name) {
   if (!name) {
     throw new Error('Name is required')
   }
-  const nameTip = (await User.find({ role: 'leader', name: new RegExp(`^${name}`) }, { name: 1, _id: 0 })
-  ).map(leader => leader.name)
+  const nameTip = (await User.find(
+    {
+      role: 'leader',
+      name: new RegExp(`^${name}`)
+    },
+    {
+      name: 1,
+      login: 12
+    })
+  ).map(({ name, login }) => ({ name, login }))
 
   return nameTip
 }

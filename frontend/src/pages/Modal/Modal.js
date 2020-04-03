ed179509c3b2396a9f2c7d2484112e71536d99d5
Stefani C. Silva <stefani.c.silva@accenture.com>
2020-04-03 12:39:26 -0300

    Import ButtonIcon component

--- frontend/src/pages/Modal/Modal.js
+++ frontend/src/pages/Modal/Modal.js
@@ -5,8 +5,9 @@ import { Title } from '../../components/Title'
 import { SubTitle, SubTitleTypes } from '../../components/SubTitle'
 import { Paragraph } from '../../components/Paragraph'
 import { Button, ButtonTypes } from '../../components/Button'
-import './Modal.scss'
 import { LogoClose } from '../../components/Logo'
+import { ButtonIcon } from '../../components/ButtonIcon'
+import './Modal.scss'
 
 import { contate, contateNumber } from '../../utils/strings'
 
@@ -30,8 +31,10 @@ function ModalPage({ isOpenModal, title, actionExit, closeModal }) {
       contentLabel={title}
       onRequestClose={actionExit}
     >
-      <div onClick={closeModal} className="button-close">
-        <LogoClose />
+      <div className="modal-header">
+        <ButtonIcon handleClick={closeModal}>
+          <LogoClose height="10" />
+        </ButtonIcon>
       </div>
       <div className="modal-content">
         <Title message={title} />

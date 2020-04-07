50bd35522ead2edfd262669f5d99c0ca111ef950
Gustavo <graf.sousa@gmail.com>
2020-04-07 09:40:01 -0300

    fix: fix dates on delivered leader screen

--- frontend/src/pages/DonationDetails/DeliveredLeader/index.js
+++ frontend/src/pages/DonationDetails/DeliveredLeader/index.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { parseISO, addDays, format } from 'date-fns'
 import PropTypes from 'prop-types'
 
 import { WaitingReceivement } from '../WaitingReceivement'
@@ -9,26 +8,15 @@ import { ShowImageButtonWithTitle } from '../../../components/ShowImageButtonWit
 
 import { receivedData, deadlineDate, legendPicDonationWithNewLine } from '../../../utils/strings'
 import { formatDate } from '../../../utils/formatDateToptbr'
+import { formatHour } from '../../../utils/formatHour'
+import { formatDateTomorrow } from '../../../utils/formaDateTomorrow'
 
 import './styles.scss'
 
 function DeliveredLeader({ current, donation }) {
   const { received } = donation
 
-  function addOneDay() {
-    const parsedDate = parseISO(received)
-    const addedDate = addDays(parsedDate, 1)
-    return format(addedDate, 'dd/MM/yyyy', {
-      timeZone: 'America/Sao_Paulo',
-    })
-  }
 
-  function getTime() {
-    const parsedDate = parseISO(received)
-    return format(parsedDate, 'HH:mm', {
-      timeZone: 'America/Sao_Paulo',
-    })
-  }
 
   return (
     <>
@@ -37,12 +25,12 @@ function DeliveredLeader({ current, donation }) {
         <div>
           <Legend type={LegendTypes.LIGHT} orientation={LegendTypes.START} message={receivedData} />
           <Legend type={LegendTypes.STRONG} orientation={LegendTypes.START} message={formatDate(received)} />
-          <Legend type={LegendTypes.STRONG} orientation={LegendTypes.START} message={getTime()} />
+          <Legend type={LegendTypes.STRONG} orientation={LegendTypes.START} message={formatHour(received)} />
         </div>
         <div>
           <Legend type={LegendTypes.LIGHT} orientation={LegendTypes.END} message={deadlineDate} />
-          <Legend type={LegendTypes.STRONG} orientation={LegendTypes.END} message={addOneDay()} />
-          <Legend type={LegendTypes.STRONG} orientation={LegendTypes.END} message={getTime()} />
+          <Legend type={LegendTypes.STRONG} orientation={LegendTypes.END} message={formatDateTomorrow(received)} />
+          <Legend type={LegendTypes.STRONG} orientation={LegendTypes.END} message={formatHour(received)} />
         </div>
       </div>
       <div className="component-deliveredLeader-imageButton">

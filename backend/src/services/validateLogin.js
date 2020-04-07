8a6cfa269ea80cc9987f18fda0c9c09335b2cb5d
Stefani C. Silva <stefani.c.silva@accenture.com>
2020-04-07 11:20:19 -0300

    Validation with Joi

--- backend/src/services/validateLogin.js
+++ backend/src/services/validateLogin.js
@@ -1,6 +1,11 @@
-export async function validateLogin (args) {
-  console.log(args.login)
-  return {
-    login: args.login
-  }
+
+const Joi = require('joi').extend(require('joi-cpf-cnpj'))
+
+export async function validateLogin (cpf) {
+  const loginSchema = Joi.document().cpf()
+  const login = Joi.validate(cpf, loginSchema)
+
+  if (login.error) return false
+
+  return true
 }

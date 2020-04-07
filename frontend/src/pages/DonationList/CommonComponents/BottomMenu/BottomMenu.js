2ef8f44b9afe659a156b439fc0023bc497cef161
Diego Griep <fale@diegogriep.com.br>
2020-04-07 16:10:56 -0300

    feat: new charge feature

--- frontend/src/pages/DonationList/CommonComponents/BottomMenu/BottomMenu.js
+++ frontend/src/pages/DonationList/CommonComponents/BottomMenu/BottomMenu.js
@@ -1,14 +1,35 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+
 import './BottomMenu.scss'
 
-import { icBasket } from '../../../../assets/icons'
+import { icBasket, icCharge } from '../../../../assets/icons'
 
-function BottomMenu() {
+function BottomMenu({ isAdmin }) {
   return (
-    <div className="containerMenu">
-      <img src={icBasket} alt="" />
-    </div>
+    <nav className="containerMenu">
+      <ul>
+        <li>
+          <button>
+            <img src={icBasket} alt="Ícone de cesta com um coração dentro" />
+          </button>
+        </li>
+
+        {isAdmin && (
+          <li>
+            <button>
+              <img src={icCharge} alt="Ícone de pasta com dados" />
+            </button>
+          </li>
+        )}
+
+      </ul>
+    </nav>
   )
+}
+
+BottomMenu.propTypes = {
+  isAdmin: PropTypes.bool
 }
 
 export default BottomMenu

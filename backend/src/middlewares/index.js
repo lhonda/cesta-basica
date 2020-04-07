5524b7a1e17f4650fdf1c16f667acbcb94bf3d59
Fernando Reis Guimaraes <fernando.g.reis@accenture.com>
2020-04-07 14:42:54 -0300

    fix admin routes

--- backend/src/middlewares/index.js
+++ backend/src/middlewares/index.js
@@ -1,4 +1,2 @@
 export { authRequired } from './auth-required'
-export { databaseConnector } from './database-connector'
 export { genericErrorHanlder } from './genric-error-handler'
-
--- backend/src/rules/details-donation.js
+++ backend/src/rules/details-donation.js
@@ -1,78 +1,89 @@
-import { Donation, User, Site } from '../repositories'
+import { Donation, User, Site, Voucher } from '../repositories'
 import { statuses } from '../enums'
 
 export async function detailsDonation ({ donationId }) {
   if (!donationId) {
-    throw new Error('DonationId is required')
+    throw new Error('donationId is required')
   }
-  try {
-    const donations = await Donation.findOne({ donationId })
-      .map(async ({
-        donationId,
-        leaderLogin,
-        siteId,
-        quantity,
-        receivedQuantity,
-        donor,
-        status,
-        receivedCardsS3Key,
-        created,
-        scheduled,
-        sentDate,
-        received,
-        lastDelivery,
-        completed,
-        strayed,
-        point
-      }) => {
-        const leader = await User.findOne({ login: leaderLogin }, { name: 1 })
 
-        if (!leader) {
-          throw new Error('Invalid leaderLogin')
-        }
+  const donation = await Donation.findOne({ donationId })
 
-        const site = await Site.findOne({ siteId }, { name: 1, city: 1, state: 1 })
-
-        if (!site) {
-          throw new Error('Invalid siteId')
-        }
-
-        const leaderName = leader.name
-        const siteName = site.name
-        const { city, state } = site
-
-        return {
-          donationId,
-          leaderLogin,
-          leaderName,
-          siteId,
-          siteName,
-          city,
-          state,
-          quantity,
-          receivedQuantity,
-          donor,
-          status,
-          statusText: statuses[status],
-          receivedCardsS3Key,
-          created,
-          scheduled,
-          sentDate,
-          received,
-          lastDelivery,
-          completed,
-          strayed,
-          point
-        }
-      })
-    return { donations }
-  } catch (error) {
-    if (error.message === 'Invalid leaderLogin') {
-      throw new Error(error.message)
-    }
-    if (error.message === 'Invalid siteId') {
-      throw new Error(error.message)
-    }
+  if (!donation) {
     throw new Error(`Does not exists donation with donationId ${donationId}`)
+  }
+
+  const {
+    leaderLogin,
+    siteId,
+    quantity,
+    receivedQuantity,
+    donor,
+    status,
+    receivedCardsS3Key,
+    created,
+    scheduled,
+    sentDate,
+    received,
+    lastDelivery,
+    completed,
+    strayed,
+    point
+  } = donation
+
+  const leader = await User.findOne({ login: leaderLogin }, { name: 1 })
+
+  if (!leader) {
+    throw new Error('Could not find the leader associate with this donation')
+  }
+
+  const site = await Site.findOne({ siteId }, { name: 1, city: 1, state: 1 })
+
+  if (!site) {
+    throw new Error('Could not find the site associate with this donation')
+  }
+
+  const publicPhotoUrl = `https://${process.env.BUCKET_NAME}.s3.amazonaws.com/${receivedCardsS3Key}`
+
+  const { name: leaderName } = leader
+
+  const { name: siteName, city, state } = site
+
+  const vouchers = (await Voucher.find({ donationId })).map(({
+    receivedCpf,
+    receivedName,
+    receivedContactNumber,
+    cardDonatedS3Key
+  }) => {
+    return {
+      receivedCpf,
+      receivedName,
+      receivedContactNumber,
+      publicPhotoUrl: `https://${process.env.BUCKET_NAME}.s3.amazonaws.com/${cardDonatedS3Key}`
+    }
+  })
+
+  return {
+    donationId,
+    leaderLogin,
+    leaderName,
+    siteId,
+    siteName,
+    city,
+    state,
+    quantity,
+    receivedQuantity,
+    donor,
+    status,
+    statusText: statuses[status],
+    publicPhotoUrl,
+    created,
+    scheduled,
+    sentDate,
+    received,
+    lastDelivery,
+    completed,
+    strayed,
+    point,
+    vouchers
   }
 }

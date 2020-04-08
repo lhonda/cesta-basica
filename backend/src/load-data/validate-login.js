e44e6235a8d723cc87f20eac512887140c73e80b
Stefani C. Silva <stefani.c.silva@accenture.com>
2020-04-08 15:02:31 -0300

    Invalid logins txt file created

--- backend/src/load-data/validate-login.js
+++ backend/src/load-data/validate-login.js
@@ -1,6 +1,8 @@
 import { config } from 'dotenv'
 import { connect, disconnect } from '../core/database'
 import { User } from '../repositories'
+import path from 'path'
+import fs from 'fs'
 
 if (require.main === module) {
   (async function () {
@@ -10,6 +12,23 @@ if (require.main === module) {
       /**
         *  Validate login as Cpf
       */
+
+      const users = await User.find()
+      // console.log(users)
+
+      /*for (const user of users) {
+        const { login } = user
+        
+        // console.log('Checking if user login is valid', await validateLogin(login))
+      }*/
+
+      const txtPath = path.resolve('data-to-load', 'invalid-logins')
+      // console.log(txtPath)
+
+      fs.writeFile(`${txtPath}`, users, function (err) {
+        if (err) throw err
+        console.log('Arquivo criado')
+      })
 
       await disconnect()
     } catch (err) {

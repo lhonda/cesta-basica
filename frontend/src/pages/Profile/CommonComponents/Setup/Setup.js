12e3d4cf7af2aa804dacfb09581ea40b302cf844
rosanaemidio <rosanaemidio42@gmail.com>
2020-04-14 17:23:19 -0300

    Add icons, components and navegation in this page

---
+++ frontend/src/pages/Profile/CommonComponents/Setup/Setup.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Setup.scss'
+
+function Setup({ action, message, icon, legend }) {
+  return (
+    <div className="container-setup">
+      <div className="container-icon">
+        <img src={icon} alt={legend} />
+      </div>
+      <a
+        onClick={action}
+      >{message}</a>
+    </div>
+  )
+}
+
+
+export default Setup;

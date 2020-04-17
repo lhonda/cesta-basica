ccf868c38aa926829aa25ce611d306b1c1227a12
Joao Lucas <jlcarruda3@gmail.com>
2020-04-17 20:26:36 -0300

    Implementação da lógica de Multipart Upload, utilizando um cursor do Mongoose como entrada de dados

--- backend/src/services/s3.js
+++ backend/src/services/s3.js
@@ -1,9 +1,78 @@
 import { S3 } from 'aws-sdk'
+const s3 = new S3()
 
 export function upload(Key, Body) {
-  return new S3().upload({
+  return s3.upload({
     Bucket: process.env.BUCKET_NAME,
     Key,
     Body
   }).promise()
+}
+
+export function createMultipartUpload(s3, Key) {
+  return s3.createMultipartUpload({
+    Bucket: process.env.BUCKET_NAME,
+    Key,
+  }).promise()
+}
+
+export async function multipartUploadCSVFromCursor(Key, cursor, transformation, batchSize = 9) {
+  try {
+    const { UploadId } = await createMultipartUpload(s3, Key)
+
+    let Body = ""
+    let count = 0
+    let PartNumber = 0
+    let uploadParts = []
+    await cursor.map(transformation).eachAsync(async doc => {
+      if (count === batchSize) {
+        count = 0
+        PartNumber += 1
+        const { ETag } = await s3.uploadPart({
+          UploadId,
+          Key,
+          Body,
+          PartNumber,
+          Bucket: process.env.BUCKET_NAME,
+        }).promise()
+        uploadParts.push({
+          PartNumber,
+          ETag
+        })
+        Body = ""
+      }
+
+      Body += `${doc}\n`
+      count += 1
+    })
+
+    if (Body.length > 0) {
+      PartNumber += 1
+      const { ETag } = await s3.uploadPart({
+        UploadId,
+        Key,
+        Body,
+        PartNumber,
+        Bucket: process.env.BUCKET_NAME,
+      }).promise()
+
+      uploadParts.push({
+        PartNumber,
+        ETag
+      })
+    }
+
+    const result = await s3.completeMultipartUpload({
+      Bucket: process.env.BUCKET_NAME,
+      Key,
+      MultipartUpload: {
+        Parts: uploadParts
+      },
+      UploadId
+    }).promise()
+    console.log(result)
+    return result
+  } catch (error) {
+    console.error(error)
+  }
 }

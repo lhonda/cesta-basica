3a027f1a4a02bd805921cec490467b22da8a3777
Leonel Pelusi <cs-leonel-pelusi@accenture.com>
2020-04-24 10:18:18 -0300

    refactor: adicionando validação ao email

--- backend/src/rules/update-user.js
+++ backend/src/rules/update-user.js
@@ -1,8 +1,8 @@
 import { User } from '../repositories/user'
 import { encrypt } from '../services/encrypt'
+import Joi from 'joi'
 
 export async function updateUser (params) {
-  console.log(params)
   let { login, email, password, confirmPassword } = params
 
   if (!email && !password) {
@@ -16,6 +16,14 @@ export async function updateUser (params) {
   }
 
   if (email) {
+    const emailSchema = Joi.object({
+      email: Joi.string().lowercase().email({ minDomainAtoms: 2 })
+    })
+    const result = Joi.validate({ email }, emailSchema)
+    if (result.error) {
+      throw new Error('Digite um e-mail válido')
+    }
+
     const newEmail = { $set: { email: email } }
 
     userFDB.updateOne(newEmail, (err, res) => {

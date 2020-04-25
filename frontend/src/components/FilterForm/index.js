62b5c059ea15aa3833642eb9c7ae10fd6687b1a8
Rafael Matoso <rafael.gustavo.gomes@concrete.com.br>
2020-04-25 09:15:04 -0300

    fix: borderoFirstLetter... to doantionFirstL..

--- frontend/src/components/FilterForm/index.js
+++ frontend/src/components/FilterForm/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { string, array, func, bool, object } from 'prop-types'
+import { string, array, func, bool } from 'prop-types'
 
 import { Loader } from '../Loader'
 import { Select } from '../Select'
@@ -13,7 +13,7 @@ import {
   chooseSite,
   cityFirstLetterCapitalized,
   statusFirstLetterCapitalized,
-  borderoFirstLetterCapitalized,
+  donationFirstLetterCapitalized,
   finalDateFirstLetterCapitalized,
   initialDateFirstLetterCapitalized,
   countryStateFirstLetterCapitalized,
@@ -82,7 +82,7 @@ function FilterForm({
             selected={borderos}
             getSelected={setBorderos}
             optionData={borderoList}
-            placeholder={borderoFirstLetterCapitalized}
+            placeholder={donationFirstLetterCapitalized}
           />
           <Select
             optionsList={states}

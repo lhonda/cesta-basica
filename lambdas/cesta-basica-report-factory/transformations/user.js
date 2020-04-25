bb9fdad152225b9f1f5dec115fee66d989f7f8ef
Joao Lucas <jlcarruda3@gmail.com>
2020-04-25 19:16:47 -0300

    Fix de parse de data para transformação de líder

--- lambdas/cesta-basica-report-factory/transformations/user.js
+++ lambdas/cesta-basica-report-factory/transformations/user.js
@@ -1,7 +1,8 @@
+const { parseDate } = require("../utils")
 
 module.exports = data => {
   const header = 'Usuário;Nome;Entidade;RG;Telefone;Email;Data de Nascimento;Endereço;Cidade;UF;CEP;Comunidades Atendidas;Tipo de Usuário\n'
-  const transformedData = `${header}\n${data.map(e => `${e.login};${e.name};${e.siteName};${e.rg};${e.phone};${e.email};${e.birthdate};${e.deliveryLocaton};${e.city};${e.state};${e.deliveryCep};${e.slums};${roleMap[e.role]}`).join('\n')}`
+  const transformedData = `${header}\n${data.map(e => `${e.login};${e.name};${e.siteName};${e.rg};${e.phone};${e.email};${parseDate(e.birthdate)};${e.deliveryLocaton || "Não Informado"};${e.city};${e.state};${e.deliveryCep};${e.slums};${roleMap[e.role]}`).join('\n')}`
 
   return { byteSize: Buffer.byteLength(transformedData), data: transformedData, length: data.length }
 }
---
+++ lambdas/cesta-basica-report-factory/utils/index.js
@@ -0,0 +1,5 @@
+const parseDate = require('./parseDate')
+
+module.exports = {
+  parseDate
+}
